fccb1519f150dd4a60f033e11735fbc9a2a97dc4
Laurent Ros <laurentr@theodo.fr>
2017-06-02 00:07:02 +0200

    [Feature] ToDo App

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,28 @@
       <span>Vue.js PWA</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
 </template>
 
 <script>
+require('material-design-lite')
 export default {
   name: 'app'
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+@import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
 body {
   margin: 0;
 }
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
 main {
-  text-align: center;
-  margin-top: 40px;
+  display: flex;
+  padding: 10px;
 }
 
 header {
---
+++ src/components/Home.vue
@@ -0,0 +1,73 @@
+<template>
+<div class="home">
+  <div v-for="task in this.tasks">
+    <div class="demo-card-wide mdl-card mdl-shadow--2dp task">
+      <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text">{{task.name}}</h2>
+      </div>
+      <div class="mdl-card__menu">
+        <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+          <i class="material-icons">share</i>
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="button-container">
+    <button class="button mdl-button mdl-js-button mdl-button--fab mdl-button--colored" @click="addTask">
+      <i class="material-icons">add</i>
+    </button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA',
+      tasks: []
+    }
+  },
+  methods: {
+    addTask: function () {
+      this.tasks.push(
+        {
+          'name': `task n°${this.tasks.length + 1}`,
+          'done': false
+        }
+      )
+    }
+  }
+}
+  </script>
+ 
+<style scoped>
+
+  .home {
+    height: 100%;
+    width: 100%;
+  }
+
+  .task {
+    min-height: 0;
+    width: initial;
+    display: flex;
+    flex: 1;
+    margin-bottom: 5px;
+  }
+
+  .button {
+    margin: 10px;
+  }
+
+  .button-container {
+    position: fixed;
+    display: flex;
+    flex-direction: row-reverse;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    z-index: 1;
+  }
+</style>
